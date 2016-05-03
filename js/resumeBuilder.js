248fa6182b1e7f2c509bcdcaf2fa3857b268de9f
Abraham Alvarez <alvarez.abraham.09@gmail.com>
2016-05-02 18:34:27 -0700

    DataTypes Section May 2
    lessons through the quiz section

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,32 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+/*
+ var email = "abraham@udacity.com";
+
+ var newEmail = email.replace("udacity", "gmail");
+
+console.log(email);
+console.log(newEmail);
+
+var awesomeThoughts = "My name is Abraham and I am AWESOME!";
+
+var funThoughts = awesomeThoughts.replace("AWESOME!","FUN");
+
+$("#main").append(funThoughts);
+*/
+var name = "Abraham Alvarez"
+var role = "Product Manager"
+
+console.log(name);
+console.log(role);
+
+var formattedName =  HTMLheaderName.replace("%data%", name);
+var formattedRole =  HTMLheaderRole.replace("%data%", role);
+
+console.log(formattedName);
+console.log(formattedRole);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
